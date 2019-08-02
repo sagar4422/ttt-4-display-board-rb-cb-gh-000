5648f2c7315a04e979057e0a6448f4ed3a06cf19
Sagar Gururaj Deshpande <sagargururaj.d@gmail.com>
2019-08-02 14:54:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,5 +8,5 @@ def display_board(board)
   puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end
 
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
 display_board(board)
