fc227ce00eaa7ab7c29db0ee38bbe3a538fb0cca
Sagar Gururaj Deshpande <sagargururaj.d@gmail.com>
2019-08-02 14:49:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  print "#{board[0]}|#{board[1]}|#{board[2]}"
+  print "--------------------"
+  print "#{board[3]}|#{board[4]}|#{board[5]}"
+  print "--------------------"
+  print "#{board[6]}|#{board[7]}|#{board[8]}"
+end
+
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+display_board(board)
